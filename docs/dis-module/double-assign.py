b26bdb3a873acd198d45d22e78d5096cf81d5b77
whitemask-1 <whitemask.exe.1@gmail.com>
2026-03-01 00:21:34 -0500

    review/reviewing chained assignment with mutable lists
    How does chained assignment in python work, what are the opcodes that
    the Cpython compiler uses, what is the evaluation stack and the block
    stack. These are all questioons that were answered by going over the
    article on chained assignment from pycoders weekly.

---
+++ docs/dis-module/double-assign.py
@@ -0,0 +1,18 @@
+import dis
+
+
+def two_pointer():
+    left = right = []
+    left.append(1)
+    return right == [1]
+
+
+def two_pointers():
+    left = []
+    right = []
+    left.append(1)
+    return right == [1]
+
+
+dis.dis(two_pointer)
+dis.dis(two_pointers)
