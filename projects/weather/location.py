64ca7d7d2638500f665e25029b9f4dba5a9d39ba
whitemask-1 <whitemask.exe.1@gmail.com>
2026-03-22 14:40:45 -0400

    dev/initial check for current location function

--- projects/weather/location.py
+++ projects/weather/location.py
@@ -1,7 +1,15 @@
 import os
 
-def load_current_location():
+def check_for_current_location():
     current_directory = os.path.dirname(__file__)
-    print(current_directory)
+    config_dir = os.path.join(current_directory, "current_location.json")
 
-load_current_location()
+    if os.path.exists(config_dir):
+        with open(config_dir) as config_file:
+            current_location = config_file.read()
+            return current_location
+    else:
+        with open(config_dir, "w") as config_file:
+            user_location = input("Enter current location")
+            config_file.write(f'"location":{user_location}')
+            return user_location
